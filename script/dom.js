c9493d2f320d31e81a38f434203dd4a71458aa76
shashriv <shashriv@publicisgroupe.net>
2019-07-31 12:08:49 +0530

    Version One is ready to use

--- script/dom.js
+++ script/dom.js
@@ -1,13 +1,37 @@
 var request = require("request");
 var jsdom = require("jsdom");
+var fs = require("fs");
+var rbl = require("remove-blank-lines");
+var dir = "./output";
+
 const { JSDOM } = jsdom;
+
+// creating a dummy html base structure
+var data = fs.readFileSync("./index.html", "utf-8");
+
+// saving credentials of the localhost for temp purpose
+const username = "admin";
+const password = "admin";
+let url = process.argv[2].split("//")[1].split("4502");
+
+url = "http://" + username + ":" + password + "@localhost:4502" + url[1];
 
 request(
   {
-    uri: "https://www.kia.com/fr/"
+    uri: url
   },
   function(error, response, body) {
     const dom = new JSDOM(body);
-    console.log(dom.window.document.querySelector(".gnb").outerHTML);
+    var htmlOutput = rbl(dom.window.document.body.innerHTML);
+    data = data.replace("{{{Replace}}}", htmlOutput);
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    fs.writeFile(`${dir}/output`, data, err => {
+      if (err) console.log(err);
+      console.log("Successfully Written to File.");
+    });
+
+    if (error) throw new Error("Something went wrong!");
   }
 );
